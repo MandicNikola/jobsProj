92e8551cc4b3e277694087956c1d2ef9a1c0b2e4
MandicNikola <mandic_nikola96@uns.ac.rs>
2019-04-23 16:03:25 +0200

    added navigation to header

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
+
+    static navigationOptions = ({ navigation }) => {
+       
+       return {
+            title: 'Review Jobs',
+            headerRight : (
+                    <Button 
+                        title='Settings'  
+                        onPress={ () => navigation.navigate('settings') } 
+                        buttonStyle = { style.buttonStyle }
+                        titleStyle = { style.titleStyle }
+                    />
+                )
+       };
+    }
+
 
     render()
     {
@@ -21,5 +38,19 @@ class ReviewScreen extends Component {
     }
 
 }
+
+
+const style = {
+    buttonStyle : {
+        backgroundColor : 'rgba(0,0,0,0)',
+        paddingRight : 20
+    },
+    titleStyle: { 
+        color: 'rgba(0, 122, 255, 1)'
+    }
+
+
+};
+
 
 export default ReviewScreen;
