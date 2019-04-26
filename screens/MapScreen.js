20dc29188137210fafa455cb17220d84012dc56f
MandicNikola <mandic_nikola96@uns.ac.rs>
2019-04-26 18:22:58 +0200

    changes on reducers and actions

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { View, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
+import { Button } from 'react-native-elements';
+
 
 class MapScreen extends Component {
 
@@ -23,6 +28,9 @@ class MapScreen extends Component {
         this.setState({ region });
     }
 
+    onButtonPress = () => {
+        this.props.fetchJobs(this.state.region);
+    }
 
     render()
     {
@@ -38,15 +46,36 @@ class MapScreen extends Component {
         }
         return (
             <View style={{ flex: 1 }}>
-              <MapView 
-                style={{ flex: 1 }} 
-                region={this.state.region}
-                onRegionChangeComplete = { this.onRegionChangeComplete }
-              />
+                <MapView 
+                    style={{ flex: 1 }} 
+                    region={this.state.region}
+                    onRegionChangeComplete = { this.onRegionChangeComplete }
+                />
+
+                <View style={ styles.buttonContainer } > 
+                    <Button  
+                        large
+                        title='Search This Area'
+                        backgroundColor = '#009688'
+                        icon={{ name: 'search' }}
+                        onPress = { this.onButtonPress }
+                    />
+                </View>
             </View>
+           
         );
     }
 
 }
 
-export default MapScreen;
+const styles = {
+    buttonContainer : {
+        position : 'absolute',
+        bottom : 20,
+        left : 10,
+        right : 10
+    }
+};
+
+
+export default connect(null, actions)(MapScreen);
